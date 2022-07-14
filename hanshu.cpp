e6a2772c2bc91fe61adbe5485a94284091894971
FL <FL@DESKTOP-08Q858E>
2022-07-14 17:17:50 +0800

    hanshu

--- hanshu.cpp
+++ hanshu.cpp
@@ -2,9 +2,23 @@
 #include<cstdlib>
 using namespace std;
 
+void swap(int num1, int num2) {
+	int temp = num1;
+	num1 = num2;
+	num2 = temp;
+}
 
+void swap1(int &num1, int &num2) {
+	int temp = num1;
+	num1 = num2;
+	num2 = temp;
+}
 
 int main() {
-
+	int a = 10, b = 20;
+	swap(a, b);
+	cout << a << b << endl;
+	swap1(a, b);
+	cout << a << b << endl;
 	return 0;
 }
